fcccc5920b26ff66f5a809d592958f96c0f8905b
Jocelyn Kaye Matsuo <jocelyn.matsuo@gmail.com>
2023-07-06 09:02:12 -0500

    Addressing PR comments

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # sumo-wrestler
 Sumo Wrestler quickly gets your reading opponents out of the ring.
 
+
+### Pre-requisites
+ - A Local PDF to summarize
+ - Install the dependencies & run pipenv shell [Getting Started](#getting-started)
+ - OpenAI API key in a .env file
+ - 
 ## Getting Started
 
 This project uses [Pipenv](https://pypi.org/project/pipenv/) for package and virtual environment management. 
@@ -23,19 +29,18 @@ exit
 ```
 (`deactivate` will mess up pipenv)
 
-
-
-## Config Map
-OPENAI_API_KEY (str) - the api key you use to connect to OpenAI
-
 ## Running the app
-### Pre-requisites
- - A Local PDF to summarize
- - Install the dependencies & run pipenv shell [Getting Started](#getting-started)
 
-### Example Call
+Call the app from the pipenv shell `pipenv shell`.
+```
+python summarize.py -f [path to file, i.e. /results/summarize/2210.03629.pdf/2210.03629.pdf]
+```
 
-Run the script, passing in the path to the pdf
-```
-python summarize.py -f [filepath]
-```
+## Config
+
+ This project uses a .env file. (Pipenv will automatically run .env on startup of the shell). 
+
+ There is a .env.sample which should have all environment variables and sample values, copy this into a file called `.env` at the root of the project. This filename is in the .gitignore. Standard WARNING about sharing your api key.
+ 
+### Config Map
+OPENAI_API_KEY (str) - the api key you use to connect to OpenAI
--- summarize.py
+++ summarize.py
@@ -5,6 +5,7 @@ from langchain.chains.summarize import load_summarize_chain
 from time import time
 import argparse
 
+# Parse arguments (-h help is automatically generated)
 parser = argparse.ArgumentParser()
 parser.add_argument(
     "-f",
@@ -21,37 +22,67 @@ parser.add_argument(
     default="map_reduce",
 )
 
+parser.add_argument(
+    "-s",
+    "--chunk_size",
+    type=int,
+    help="Size of the chunks to split the PDF into",
+    default=750,
+)
+
+parser.add_argument(
+    "-o",
+    "--chunk_overlap",
+    help="Amount of overlap between split chunks of PDF",
+    default=100,
+)
+
 args = parser.parse_args()
 
 
-def summarize(pdf_path, chain_type):
-    # Load the PDF
+def summarize(pdf_path, chain_type, chunk_size, chunk_overlap) -> str:
+    """Summarize a pdf using openai
+    Takes a
+    Required File path to a pdf,
+    Optional chain_type (strategy to call openai to generate summary)
+    Optional chunk_size (size of the chunks to split the pdf into)
+    Optional chunk_overlap (amount of overlap between chunks)
+    Returns a summary of the pdf from openai
+    """
+
+    # Load the PDF from file_path
     print(f"Loading PDF from path: {pdf_path}")
     loader = PyPDFLoader(file_path=pdf_path)
 
     # Split the PDF into chunks of text
     text_splitter = CharacterTextSplitter(
-        chunk_size=750, chunk_overlap=100, separator="/n"
+        chunk_size=chunk_size, chunk_overlap=chunk_overlap, separator="/n"
     )
     document = loader.load()
     docs = text_splitter.split_documents(documents=document)
 
-    # Summarize throught openai
+    # Summarize throught openai, use the chain_type specified
     llm = OpenAI(temperature=0)
     chain = load_summarize_chain(llm, chain_type=chain_type)
     openai_response = chain.run(docs)
+
+    # todo: store the summary in a file
 
     return openai_response
 
 
 if __name__ == "__main__":
-    chain_type = "map_reduce"
-    pdf_path = "/Users/jocelynmatsuo/devdir/lab/vectorsource-in-memory/2210.03629.pdf"
-
     start = (
         time()
     )  # timing the method, because certain chain_types take a super long time
 
-    print(summarize(pdf_path=args.file_path, chain_type=args.chain_type))
+    print(
+        summarize(
+            pdf_path=args.file_path,
+            chain_type=args.chain_type,
+            chunk_overlap=args.chunk_overlap,
+            chunk_size=args.chunk_size,
+        )
+    )
 
-    print(f"Time to summarize using chain_type = {chain_type}: {time() - start}")
+    print(f"Time to summarize using chain_type = {args.chain_type}: {time() - start}")
